bf1b85d266f52d9f80d183acb8993426f89684a7
tyler reitz <tdreitz@gmail.com>
2021-12-22 11:30:56 -0800

    Create a basic hello world web application

---
+++ server.js
@@ -0,0 +1,16 @@
+const { hostname } = require('os')
+const http = require('http')
+
+const MESSAGE = 'Hello World\n'
+const PORT = 8080
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end(MESSAGE)
+})
+
+server.listen(PORT, hostname, () => {
+  console.log(`Server running at http://${hostname}:${PORT}`)
+})
+

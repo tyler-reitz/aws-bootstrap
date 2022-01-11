631b91f3245992d675d165207506f787432827c8
tyler reitz <tdreitz@gmail.com>
2022-01-11 14:15:08 -0800

    Fix npm start script

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { hostname } = require('os')
 const http = require('http')
-const STACK_NAME = process.env.STACK_NAME || "Unknown Stack"
 
+const STACK_NAME = process.env.STACK_NAME || "Unknown Stack"
 const MESSAGE = `Hello World from ${hostname()}\n in ${STACK_NAME}\n`
 const PORT = 8080
 

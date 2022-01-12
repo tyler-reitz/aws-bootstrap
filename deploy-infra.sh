a20e3c6bda389fca4b0d65d694a47d9b758e622d
tyler reitz <tdreitz@gmail.com>
2022-01-12 11:53:26 -0800

    Split out staging nested stack

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -12,6 +12,7 @@ GH_BRANCH=master
 
 AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
 
 echo $CODEPIPELINE_BUCKET
 
@@ -24,7 +25,25 @@ aws cloudformation deploy \
   --template-file setup.yaml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET \
+    CloudFormationBucket=$CFN_BUCKET
+
+# Package up CloudFormation templates into an S3 bucket
+echo -e "\n\n=========== Packaging main.yaml ============"
+mkdir -p ./cfn_output
+
+PACKAGE_ERR="$(aws cloudformation package \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --template main.yml \
+  --s3-bucket $CFN_BUCKET \
+  --output-template-file ./cfn_output/main.yml 2>&1)"
+
+if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+  echo "ERROR while running 'aws cloudformation package' command:"
+  echo $PACKAGE_ERR
+fi
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml =============="
@@ -32,7 +51,7 @@ aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
   --stack-name $STACK_NAME \
-  --template-file main.yml \
+  --template-file ./cfn_output/main.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
@@ -58,8 +77,10 @@ aws cloudformation deploy \
 #     ParameterKey=GitHubPersonalAccessToken,ParameterValue=$GH_ACCESS_TOKEN \
 #     ParameterKey=CodePipelineBucket,ParameterValue=$CODEPIPELINE_BUCKET
 
+# If the deploy succeeded, show the DNS name of the endpoints
 if [ $? -eq 0 ]; then
   aws cloudformation list-exports \
     --profile awsbootstrap
     --query "Exports[?ends_with(Name,'LBEndpoint')].Value"
 fi
+

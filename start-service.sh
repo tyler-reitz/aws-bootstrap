6c87227bac271dfde4bc42bf75114b191e822515
tyler reitz <tdreitz@gmail.com>
2022-01-12 08:58:35 -0800

    Fix region in start-script

--- start-service.sh
+++ start-service.sh
@@ -4,10 +4,10 @@ source /home/ec2-user/.bash_profile
 cd /home/ec2-user/app/release
 
 # Qeury the EC2 metadata service for this instances's region
-REGION="`wget -qO- http://instance-data/latest/meta-data/placement/availability-zone | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+REGION="`wget -qO- http://instance-data/latest/meta-data/placement/region
 
 # Query the EC2 metadata service for this instance's instance-id
-export INSTANCE_ID="`wget -q -O http://169.254.169.254/latest/meta-data/instance-id`"
+export INSTANCE_ID="`wget -q -O - http://169.254.169.254/latest/meta-data/instance-id`"
 
 # Query EC2 describeTags method and pull out the CFN logical ID for this instance
 export STACK_NAME=`aws --region $REGION ec2 describe-tags \
